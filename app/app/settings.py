f56c06efba1d79f94c170bfb43012c4e22f4de37
jamwine <shauryakhurana07@gmail.com>
2022-02-26 14:39:19 -0600

    Revert "Custom User Model"
    This reverts commit 9f98b9791f0b67167132a40a1f7304cd775040a2.

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
 ]
 
 MIDDLEWARE = [
@@ -127,5 +126,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-AUTH_USER_MODEL = 'core.User'
