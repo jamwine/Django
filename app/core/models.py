c02a78dd539c256dbc36f6f5a3096a6bba1dfe24
jamwine <shauryakhurana07@gmail.com>
2022-03-06 03:32:20 -0600

    Pushing .env file for demo purpose

--- app/core/models.py
+++ app/core/models.py
@@ -9,8 +9,7 @@ class UserManager(BaseUserManager):
         """Creates and saves a new user"""
         if not email:
             raise ValueError('Users must have an email address')
-        email = self.normalize_email(email)
-        user = self.model(email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
@@ -27,7 +26,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """Custom user model that suppors using email instead of username"""
+    """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
@@ -36,4 +35,3 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    # REQUIRED_FIELDS = ['name']
