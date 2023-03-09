a9b11c5a904612f1a4f43654e59d4ba363d9a6ad
JAMWINE <shauryakhurana07@gmail.com>
2023-03-09 08:28:01 -0600

    Adding portofilo_using_django branch

--- django_web/web/settings.py
+++ django_web/web/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'blog',
+    'portfolio',
 ]
 
 MIDDLEWARE = [
@@ -124,6 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# MEDIA_URL = 'data/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'data')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
